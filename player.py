0b67ad4083266746de482479d444544073a6012b
crybyte <simpleurbanart@gmail.com>
2025-05-17 04:57:37 +0000

    Added cooldown to blaster

--- player.py
+++ player.py
@@ -3,12 +3,13 @@ import pygame
 from circleshape import CircleShape
 from shot import Shot
 
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -26,6 +27,8 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.timer -= dt
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -48,9 +51,12 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if (self.timer > 0):
+            return
+        
         newShot = Shot(self.position.x, self.position.y)
-        print(f"DEBUG: {newShot}")
-        print(f"1DEBUG: {newShot.velocity}")
+        #print(f"DEBUG: {newShot}")
+        #print(f"1DEBUG: {newShot.velocity}")
         newShot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
-
+        self.timer = PLAYER_SHOOT_COOLDOWN
